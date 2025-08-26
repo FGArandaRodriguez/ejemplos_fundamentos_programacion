05856c457e75315fc4d0a98ff5c0d4f6c916ed4c
giovanniarandarodriguez <franklingiovanni97@gmail.com>
2025-08-26 07:56:01 -0500

    ADD: Ejemplos funciones que regresan valor tema 12

---
+++ funciones_que_regresan_valor.py
@@ -0,0 +1,27 @@
+# def suma(a,b,c):
+#     return a+b+c
+
+# def resta(a,b,c):
+#     return a-b-c
+
+
+# def numeros_pares(n):
+#     pares = []
+#     for i in range(1, n+1):
+#         if i % 2 == 0:
+#             pares.append(i)
+#     return pares
+
+# def realizar_operacion():
+#     uno = int(input("Ingrese un numero: "))
+#     dos = int(input("Ingrese otro numero: "))
+#     tres = int(input("Ingrese otro numero: "))
+
+#     print("Suma:",suma(uno,dos,tres))
+#     print("Resta:",resta(uno,dos,tres))
+#     print("Pares:",numeros_pares(uno))
+
+#     return "Operaciones realizadas"
+    
+
+# print(realizar_operacion())
